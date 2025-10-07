4a5d298aa47a2194cb76ae1db909d77badc2c598
professionalowo <martin.brandl1307@gmail.com>
2025-10-07 10:57:09 +0200

    refactor: move token conversion logic to separate function for clarity

--- src/jit/parser.zig
+++ src/jit/parser.zig
@@ -7,21 +7,24 @@ pub fn parseAlloc(allocator: Allocator, code: []const u8) ![]const Token {
 
     var i: usize = 0;
     for (code) |c| {
-        const token: ?Token = switch (c) {
-            '+' => .{ .plus = 1 },
-            '-' => .{ .minus = 1 },
-            '>' => .{ .inc = 1 },
-            '<' => .{ .dec = 1 },
-            '.' => .putc,
-            ',' => .getc,
-            '[' => .lparen,
-            ']' => .rparen,
-            else => null,
-        };
-        if (token) |tok| {
+        if (charToToken(c)) |tok| {
             tokens[i] = tok;
             i += 1;
         }
     }
     return tokens[0..i];
+}
+
+fn charToToken(char: u8) ?Token {
+    return switch (char) {
+        '+' => .{ .plus = 1 },
+        '-' => .{ .minus = 1 },
+        '>' => .{ .inc = 1 },
+        '<' => .{ .dec = 1 },
+        '.' => .putc,
+        ',' => .getc,
+        '[' => .lparen,
+        ']' => .rparen,
+        else => null,
+    };
 }
