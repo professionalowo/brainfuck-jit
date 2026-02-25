441f14e00b5e0650266a3e1d4580e47b60ecdd4b
tresonic <tresonic@mail.de>
2026-02-25 21:05:29 +0100

    add dummy backends

--- src/jit/jit.zig
+++ src/jit/jit.zig
@@ -3,7 +3,7 @@ const token = @import("frontend");
 
 const arch_backend = switch (builtin.target.cpu.arch) {
     .x86, .x86_64 => @import("x86/backend.zig"),
-    .arm, .aarch64 => @compileError("cpu arch not supported for jit-compilation"),
+    .arm, .aarch64 => @import("arm/backend.zig"),
     else => @compileError("cpu arch not supported for jit-compilation"),
 };
 
