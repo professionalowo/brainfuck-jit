80e12ac2ce601e740eadd2f958514523b6dc4d41
tresonic <tresonic@mail.de>
2026-02-25 20:01:54 +0100

    fix: possibly returning not freeable array

--- src/jit/parser.zig
+++ src/jit/parser.zig
@@ -4,6 +4,7 @@ const Token = @import("token").Token;
 
 pub fn parseAlloc(allocator: Allocator, code: []const u8) Allocator.Error![]const Token {
     var tokens = try allocator.alloc(Token, code.len);
+    defer allocator.free(tokens);
 
     var i: usize = 0;
     for (code) |c| {
@@ -12,7 +13,7 @@ pub fn parseAlloc(allocator: Allocator, code: []const u8) Allocator.Error![]cons
             i += 1;
         }
     }
-    return tokens[0..i];
+    return allocator.dupe(Token, tokens[0..i]);
 }
 
 fn charToToken(char: u8) ?Token {
