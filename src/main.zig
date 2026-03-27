605ac49b72c7abbd4dc221a1b2b2fc14246ff56c
tresonic <tresonic@mail.de>
2026-03-27 21:38:21 +0100

    add jit test

--- src/main.zig
+++ src/main.zig
@@ -62,3 +62,32 @@ test "interpreter_helloworld" {
     try interpreter.run(optimized, &writer, &reader);
     try std.testing.expectEqualStrings(expected, writer.buffer[0..writer.end]);
 }
+
+test "jit_helloworld" {
+    const program = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+    const expected = "Hello World!\n";
+
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
+    const dbg_allocator = gpa.allocator();
+    defer {
+        const deinit_status = gpa.deinit();
+        if (deinit_status == .leak) std.testing.expect(false) catch @panic("TEST FAIL: leaked memory");
+    }
+
+    const tokens = try parser.parseAlloc(dbg_allocator, program);
+    defer dbg_allocator.free(tokens);
+
+    const optimized = try optimizer.optimizeAlloc(dbg_allocator, tokens);
+    defer dbg_allocator.free(optimized);
+
+    var reader = std.Io.Reader.failing;
+
+    var buffer = [_]u8{0} ** 4096;
+    var writer = std.Io.Writer.fixed(&buffer);
+
+    var compiled = try jit.compile(dbg_allocator, optimized, &writer, &reader);
+    defer compiled.deinit();
+
+    try jit.Runner.run(compiled.binary.items);
+    try std.testing.expectEqualStrings(expected, writer.buffer[0..writer.end]);
+}
