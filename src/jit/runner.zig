33bef7847ec6d52f2495d7e91faab47ec3238840
professionalowo <martin.brandl1307@gmail.com>
2026-04-16 09:04:05 +0200

    propagate map error

--- src/jit/runner.zig
+++ src/jit/runner.zig
@@ -2,16 +2,14 @@ const std = @import("std");
 const AssemblerContext = @import("context.zig").AssemblerContext;
 
 pub fn run(code: []const u8) !void {
-    const executable_memory = std.posix.mmap(
+    const executable_memory = try std.posix.mmap(
         null,
         code.len,
         .{ .READ = true, .WRITE = true, .EXEC = false },
         .{ .ANONYMOUS = true, .TYPE = .PRIVATE },
         -1,
         0,
-    ) catch |err| {
-        std.debug.panic("Failed to mmap executable memory: {}", .{err});
-    };
+    );
     defer std.posix.munmap(executable_memory);
 
     try std.process.protectMemory(executable_memory, .{ .read = true, .write = false, .execute = true });
