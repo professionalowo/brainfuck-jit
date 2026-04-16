da63dc9a811b34ac875ed668f2eaec6d529e4cff
professionalowo <martin.brandl1307@gmail.com>
2026-04-16 09:02:43 +0200

    update mmap with unmap and mprotect so macos doesnt cry

--- src/jit/runner.zig
+++ src/jit/runner.zig
@@ -5,13 +5,16 @@ pub fn run(code: []const u8) !void {
     const executable_memory = std.posix.mmap(
         null,
         code.len,
-        .{ .READ = true, .WRITE = true, .EXEC = true },
+        .{ .READ = true, .WRITE = true, .EXEC = false },
         .{ .ANONYMOUS = true, .TYPE = .PRIVATE },
         -1,
         0,
     ) catch |err| {
         std.debug.panic("Failed to mmap executable memory: {}", .{err});
     };
+    defer std.posix.munmap(executable_memory);
+
+    try std.process.protectMemory(executable_memory, .{ .read = true, .write = false, .execute = true });
 
     @memcpy(executable_memory, code);
 
